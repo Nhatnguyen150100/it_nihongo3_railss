30179229ddf777711b6613563c1dd5562b214967
nhatnguyen150100 <nhatnguyen150100@gmail.com>
2021-12-07 14:18:31 +0700

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   get 'users/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get 'static_pages/home'
+  devise_for :users
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+  resources :books
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
